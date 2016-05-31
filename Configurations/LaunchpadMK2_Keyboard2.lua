947f3909ea7089f3137bec43f60ff2e3b2009680
dplduffy <dplduffy@Gmail.com>
2016-05-31 19:21:31 -0400

    added keyboard layout
    will need to change keyboard duplex files a little I think

---
+++ Configurations/LaunchpadMK2_Keyboard2.lua
@@ -0,0 +1,103 @@
+--[[----------------------------------------------------------------------------
+-- Duplex.Launchpad 
+----------------------------------------------------------------------------]]--
+
+duplex_configurations:insert {
+
+  -- configuration properties
+  name = "Keyboard2",
+  pinned = true,
+
+  -- device properties
+  device = {
+    class_name = "LaunchpadMK2",
+    display_name = "Launchpad MK2",
+    device_port_in = "Launchpad MK2",
+    device_port_out = "Launchpad MK2",
+    control_map = "Controllers/LaunchpadMK2/Controlmaps/LaunchpadMK2_Keyboard2.xml",
+    thumbnail = "Controllers/LaunchpadMK2/LaunchpadMK2.bmp",
+    protocol = DEVICE_PROTOCOL.MIDI,
+  },
+
+  applications = {
+    KeyboardLarge = {
+      application = "Keyboard",
+      mappings = {
+        key_grid = {
+          group_name = "LargeGrid",
+          --orientation = ORIENTATION.VERTICAL,
+        },
+        octave_up = {
+          group_name = "Controls",
+          index = 1
+        },
+        octave_down = {
+          group_name = "Controls",
+          index = 2
+        },
+        cycle_layout = {
+          group_name = "Controls",
+          index = 7
+        },
+
+      },
+      palette = {
+        key_pressed = {
+          color = {0x00,0xFF,0x00}
+        },
+        key_released = {
+          color = {0x66,0xFF,0xFF}
+        },
+        key_released_content = {
+          color = {0xFF,0x00,0xFF}
+        },
+        key_released_selected = {
+          color = {0x00,0x00,0x00}
+        },
+      },
+      options = {
+      },
+      hidden_options = {  -- display minimal set of options
+        "channel_pressure","pitch_bend","release_type","button_width","button_height","mod_wheel","velocity_mode","keyboard_mode"
+      },
+    },
+    Instrument = {
+      mappings = {
+        prev_scale = {
+          group_name = "Controls",
+          index = 3,
+        },
+        next_scale = {
+          group_name = "Controls",
+          index = 4,
+        },
+        set_key = {
+          group_name = "Triggers",
+          orientation = ORIENTATION.VERTICAL
+        },
+      }
+    },
+    TrackSelector = {
+      mappings = {
+        prev_track = {
+          group_name = "Controls",
+          index = 5,
+        },
+        next_track = {
+          group_name = "Controls",
+          index = 6,
+        },
+
+      }
+    },
+    Transport = {
+      mappings = {
+        edit_mode = {
+          group_name = "Controls",
+          index = 8,
+        },
+      },
+    },
+  }
+}
+
--- LaunchpadMK2.lua
+++ LaunchpadMK2.lua
@@ -15,101 +15,28 @@ A device-specific class
 
 class "LaunchpadMK2" (MidiDevice)
 
-LaunchpadMK2_COLOR_HEX = 
+LaunchpadMK2_COLOR = 
 {
-  {0x00,0x00,0x00}, --BLACK
-  {0x80,0x80,0x80}, --GREY_LO
-  {0xC0,0xC0,0xC0}, --GREY_MD
-  {0xFF,0xFF,0xFF}, --WHITE
-  {0xF0,0x80,0x80}, --ROSE
-  {0xFF,0x00,0x00}, --RED_HI
-  {0xB2,0x22,0x22}, --RED
-  {0x8B,0x00,0x00}, --RED_LO
-  {0xFF,0x8C,0x00}, --ORANGE_HI
-  {0xFF,0xA5,0x00}, --ORANGE
-  {0xF5,0xDE,0xB3}, --TAN
-  {0xFF,0xFF,0x00}, --YELLOW HI
-  {0xAD,0xFF,0x2F}, --YELLOW GREEN
-  {0x00,0xFF,0x00}, --LIME
-  {0x98,0xFB,0x98}, --LIME GREEN
-  {0x00,0x80,0x00}, --GREEN HI
-  {0x00,0x64,0x00}, --GREEN LO
-  {0x00,0xFF,0x7F}, --GREEN SPRING
-  {0x00,0xFA,0x9A}, --SPRING
-  {0x22,0x8B,0x22}, --FOREST GREEN
-  {0x00,0xFF,0x7F}, --SPRING TURQUOISE
-  {0xE0,0xFF,0xFF}, --TURQUOISE CYAN
-  {0x00,0xCC,0x99}, --CYAN HI
-  {0x53,0xC6,0x8C}, --CYAN
-  {0x39,0xAC,0x73}, --CYAN LO
-  {0x80,0xFF,0xCC}, --CYAN SKY
-  {0x00,0xFF,0xFF}, --SKY HI
-  {0x00,0xCC,0xCC}, --SKY LO
-  {0xB3,0xFF,0xFF}, --SKY OCEAN
-  {0x00,0xCC,0xFF}, --OCEAN HI
-  {0x00,0xA3,0xCC}, --OCEAN LO
-  {0x00,0x66,0xFF}, --BLUE HI
-  {0x00,0x00,0x99}, --BLUE LO
-  {0xCC,0x99,0xFF}, --BLUE ORCHID
-  {0x99,0x99,0xFF}, --ORCHID HI
-  {0x73,0x33,0xFF}, --ORCHID
-  {0xFF,0x99,0xFF}, --ORCHID MAGENTA
-  {0xFF,0x66,0xFF}, --MAGENTA HI
-  {0xFF,0x00,0xFF}, --MAGENTA
-  {0xFF,0xCC,0xFF}, --MAGENTA_LO
-  {0xFF,0xCC,0xCC}, --MAGENTA_PINK
-  {0xFF,0x33,0x99}, --PINK_HI
-  {0xCC,0x33,0x99}, --PINK_LO
+  {0x00,0x00,0x00,0}, --BLACK
+  {0x80,0x80,0x80,1}, --GREY
+  {0xFF,0xFF,0xFF,3}, --WHITE
+  {0xF0,0x80,0x80,4}, --ROSE
+  {0xFF,0x00,0x00,5}, --RED
+  {0x8B,0x00,0x00,7}, --DARK_RED
+  {0xFF,0x8C,0x00,9}, --ORANGE
+  {0xF5,0xDE,0xB3,12}, --TAN
+  {0xFF,0xFF,0x00,13}, --YELLOW
+  {0xAD,0xFF,0x2F,17}, --YELLOW GREEN
+  {0x00,0xFF,0x00,21}, --GREEN
+  {0x66,0xFF,0x99,29}, --GREEN SPRING
+  {0x66,0xFF,0xCC,33}, --SPRING
+  {0x66,0xFF,0xFF,37}, --LIGHT LIGHT BLUE
+  {0x00,0xFF,0xFF,41}, --LIGHT BLUE
+  {0x00,0x00,0xFF,45}, --BLUE
+  {0x99,0x33,0xFF,49}, --BLUE PURPLE
+  {0xFF,0x00,0xFF,53}, --PINK PURPLE
+  {0xFF,0x33,0x99,57}, --BRIGHT PINK
 }
-
-LaunchpadMK2_COLOR_DEC = 
-{
-  0;
-  1;
-  2;
-  3;
-  4;
-  5;
-  6;
-  7;
-  9;
-  10;
-  12;
-  13;
-  16;
-  17;
-  20;
-  21;
-  22;
-  25;
-  27;
-  29;
-  32;
-  33;
-  34;
-  35;
-  36;
-  37;
-  39;
-  40;
-  41;
-  43;
-  45;
-  47;
-  48;
-  49;
-  50;
-  52;
-  53;
-  54;
-  55;
-  56;
-  57;
-  59;
-}
-
-LaunchpadMK2_NUM_COLORS = 43;
-
 
 function LaunchpadMK2:__init(display_name, message_stream, port_in, port_out)
   TRACE("LaunchpadMK2:__init", display_name, message_stream, port_in, port_out)
@@ -148,61 +75,52 @@ function LaunchpadMK2:output_value(pt,xarg,ui_obj)
     --else
   
     --print("launchpad output value...",rprint(pt.color))
-
-    --default color is off
-    local rslt = 0
-    local skip_hardware = false
-    local compare_array = {} 
-    local sysex_num = nil
-    local sysex_color = {}
     
-    sysex_num = self:extract_midi_cc(xarg.value)
-    if (sysex_num == nil) then
-        sysex_num = self:extract_midi_note(xarg.value)
-    end
-
-    --print(sysex_num)
+    --This code uses sysex to output color values to the launchpad
+    --it didn't work very well because the RGB color on the screen
+    --never really lined up with the color the Launchpad displayed
+    --due to LED differences.
     
-    for i=1, 3 do
-      sysex_color[i] = math.floor((pt.color[i]/255)*63)
-    end
+    --local sysex_num = nil
+    --local sysex_color = {}
+    --sysex_num = self:extract_midi_cc(xarg.value)
+    --if (sysex_num == nil) then
+    --    sysex_num = self:extract_midi_note(xarg.value)
+    --end
     
-    --print('1',pt.color[1])
-    --print('2',pt.color[2])
-    --print('3',pt.color[3])
+    --for i=1, 3 do
+    --  sysex_color[i] = math.floor((pt.color[i]/255)*63)
+    --end
+    
+    print('num',self:extract_midi_note(xarg.value))
+    print('1',pt.color[1])
+    print('2',pt.color[2])
+    print('3',pt.color[3])
     --print('sys1',sysex_color[1])
     --print('sys2',sysex_color[2])
     --print('sys3',sysex_color[3])
     
-    local smallest_val = 675
-    local smallest_key = 0
+    local skip_hardware = false  --set to true to use sysex values
+    local output_color = 0
     
-    for i=1, LaunchpadMK2_NUM_COLORS do
-        compare_array[i] = find_color_difference(pt.color,LaunchpadMK2_COLOR_HEX[i])
-        if compare_array[i] < smallest_val then
-            smallest_val = compare_array[i]
-            smallest_key = i
+    local smallest_compare = 765
+    
+    for i=1, table.maxn(LaunchpadMK2_COLOR) do
+      local compare_value = 0
+        for j=1,3 do
+          local temp_compare = 0
+          temp_compare = math.abs(pt.color[j]-LaunchpadMK2_COLOR[i][j])
+          compare_value = temp_compare+compare_value
         end
+      if compare_value < smallest_compare then
+        smallest_compare = compare_value
+        output_color = LaunchpadMK2_COLOR[i][4]
+      end
     end
     
-    rslt = LaunchpadMK2_COLOR_DEC[smallest_key]
-    
-    --print("rslt",rslt)
     --self:send_sysex_message(0x00,0x20,0x29,0x02,0x18,0x0B,sysex_num,sysex_color[1],sysex_color[2],sysex_color[3])
     
-    return rslt,skip_hardware
-end
-
-function find_color_difference(array1,array2)
-    local compare_array = {}
-    local compare_num = nil
-    
-    for i=1,3 do
-        compare_array[i]=math.abs(array1[i]-array2[i])
-    end
-    
-    compare_num = (compare_array[1]+compare_array[2]+compare_array[3])
-    return compare_num
+    return output_color,skip_hardware
 end
 
 --------------------------------------------------------------------------------
